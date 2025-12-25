0552411427acf23f1c3876584d2c34ce6a087f35
Tiny-B <kiefkostello@gmail.com>
2025-12-25 16:11:03 +0000

    Add extra links

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import './CSS/Footer.css';
+
+export default function Footer() {
+	return (
+		<footer>
+			<p>© Trillians Rock Bar</p>
+			<ul className='footer-links'>
+				<li>f</li>
+				<li>t</li>
+				<li>i</li>
+				<li>e</li>
+			</ul>
+		</footer>
+	);
+}
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,4 +1,5 @@
 import NavHeader from '../components/NavHeader';
+import Footer from '../components/Footer';
 import './CSS/Landing.css';
 import logo from '../../public/trills-logo.png';
 
@@ -6,7 +7,7 @@ export default function Landing() {
 	return (
 		<>
 			<NavHeader />
-			<div className='main-container'>
+			<div className='main-container container-theme'>
 				<img
 					className='logo'
 					src={logo}
@@ -14,31 +15,62 @@ export default function Landing() {
 				/>
 				<p>Thank you all for your continued support!</p>
 				<p>┻━┻ ︵ ＼( °□° )／ ︵ ┻━┻</p>
-				<p>Opening Times:</p>
-				<ul className='opening-times'>
-					<li>
-						<p>Monday → 3pm - 11pm</p>
-					</li>
-					<li>
-						<p>Tuesday → 3pm - 11pm</p>
-					</li>
-					<li>
-						<p>Wednesday → 3pm - 11pm</p>
-					</li>
-					<li>
-						<p>Thursday → 12pm - 11pm</p>
-					</li>
-					<li>
-						<p>Friday → 12pm - 1am</p>
-					</li>
-					<li>
-						<p>Saturday → 12pm - 2am</p>
-					</li>
-					<li>
-						<p>Sunday → 5pm - 11pm</p>
-					</li>
-				</ul>
+				<div className='opening-times-container'>
+					<p style={{ marginBottom: '0', textAlign: 'center' }}>Opening Times:</p>
+					<ul className='opening-times-list'>
+						<li>
+							<p>Monday → 3pm - 11pm</p>
+						</li>
+						<li>
+							<p>Tuesday → 3pm - 11pm</p>
+						</li>
+						<li>
+							<p>Wednesday → 3pm - 11pm</p>
+						</li>
+						<li>
+							<p>Thursday → 12pm - 11pm</p>
+						</li>
+						<li>
+							<p>Friday → 12pm - 1am</p>
+						</li>
+						<li>
+							<p>Saturday → 12pm - 2am</p>
+						</li>
+						<li>
+							<p>Sunday → 5pm - 11pm</p>
+						</li>
+					</ul>
+				</div>
 			</div>
+			<div className='extra-container container-theme'>
+				<div className='extra-links'>
+					<a href='#'>Facebook</a>
+					<a href='#'>Twitter</a>
+					<a href='#'>Instagram</a>
+					<a href='#'>LOST & FOUND</a>
+				</div>
+				<div className='friend-links'>
+					<h2>Some of our friends</h2>
+					<ul className='friend-links-list'>
+						<li>
+							<a href='#'>THE RENEGADE ROCK SHOW</a>
+						</li>
+						<li>
+							<a href='#'>SHOCK CITY</a>
+						</li>
+						<li>
+							<a href='#'>SD ENTERTAINMENT</a>
+						</li>
+						<li>
+							<a href='#'>FLOOR SHOW PROMOTIONS</a>
+						</li>
+						<li>
+							<a href='#'>CONQUEST MUSIC</a>
+						</li>
+					</ul>
+				</div>
+			</div>
+			<Footer />
 		</>
 	);
 }
